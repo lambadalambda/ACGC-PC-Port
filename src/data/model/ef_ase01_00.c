bfb2768ab8e01f85138cfa9fdfb8d5ba0e5102ff
FlyingMeta <flyngmt@users.noreply.github.com>
2026-03-25 00:52:52 +0300

    Fix zigzag artifacts in sweat bubble effect

--- src/data/model/ef_ase01_00.c
+++ src/data/model/ef_ase01_00.c
@@ -6,7 +6,7 @@
 #include "ef_effect_control.h"
 
 #ifdef TARGET_PC
-static u8 ef_ase01_0[0x80] ATTRIBUTE_ALIGN(32);
+static u8 ef_ase01_0[0x100] ATTRIBUTE_ALIGN(32);
 #else
 static u8 ef_ase01_0[] ATTRIBUTE_ALIGN(32) = {
 #include "assets/ef_ase01_00/ef_ase01_0.inc"
